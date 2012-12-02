259fbdd675bcc8329486b1e579833449d963ebde
Frank Hoffsummer <frank.hoffsummer@gmail.com>
2012-12-02 14:40:56 +0100

    normalizing urls on save

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-
 from main.models import WebSite
 
+class WebSiteOptions(admin.ModelAdmin):
+    ordering = ['url']
+    save_on_top = True
 
-admin.site.register(WebSite)
+admin.site.register(WebSite, WebSiteOptions)
--- main/models.py
+++ main/models.py
@@ -6,9 +6,10 @@ class WebSite(models.Model):
     url = models.CharField('URL för webbsidan', max_length=250)
 
     def __unicode__(self):
-        return self.url.lstrip('http://').lstrip('www.')
+        return self.url.lstrip('http://').lstrip('www.').rstrip('/')
 
     def save(self, *args, **kwargs):
+        self.url = self.url.rstrip('/')
         if not "http://" in self.url:
             self.url = "http://" + self.url
         super(WebSite, self).save(*args, **kwargs) # Call the "real" save() method.
