e8f6235e8186c51c6e71fc9c1b54fc478542b01d
Frank Hoffsummer <frank.hoffsummer@gmail.com>
2012-12-02 16:38:18 +0100

    django templates with bootstrap

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 #-*- coding: utf-8 -*-
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
 
 def home(request):
-    return HttpResponse("Hi")
+    return render_to_response('test.html', {}, context_instance=RequestContext(request))
