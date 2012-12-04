72ca18ab60962f3158e4ad7ce31661ac981c3714
Frank Hoffsummer <frank.hoffsummer@gmail.com>
2012-12-04 21:47:06 +0100

    listing sites for a given date

--- main/views.py
+++ main/views.py
@@ -5,24 +5,29 @@ import os
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 import sys
+import datetime
+
 
 domain = "http://d2np6cnk6s6ggj.cloudfront.net/"
+AWS_ACCESS_KEY = os.environ["AWS_ACCESS_KEY"]
+AWS_SECRET_KEY = os.environ["AWS_SECRET_KEY"]
+
+def get_sites_for_day(selected_day):
+    conn = S3Connection(AWS_ACCESS_KEY, AWS_SECRET_KEY)
+    bucket = conn.get_bucket('svti-webscreenshots')
+    selected_day = selected_day.strftime("%Y/%m/%d")
+    keys = bucket.get_all_keys(prefix=selected_day + "/", delimiter="/")
+    sites = [ key.name.lstrip(selected_day).rstrip("/").replace('|', '/') for key in keys ]
+    return sites
+
 
 def home(request):
-    AWS_ACCESS_KEY = os.environ["AWS_ACCESS_KEY"]
-    AWS_SECRET_KEY = os.environ["AWS_SECRET_KEY"]
     conn = S3Connection(AWS_ACCESS_KEY, AWS_SECRET_KEY)
     bucket = conn.get_bucket('svti-webscreenshots')
     domain = bucket.get_website_endpoint()
-    rs = bucket.list("2012/12/02/tv4")
-    fns = [ key.name.lstrip("2012/12/02") for key in bucket.list("2012/12/02/tv4") ]
-    d = {}
-    for fn in fns:
-        parts = fn.split('-')
-        timestamp = parts[-2]
-        site = "/".join(parts[:-2])
-        print site
-        print timestamp
+    sites = get_sites_for_day(datetime.date.today())
+    return render_to_response('test.html', {'sites': sites}, context_instance=RequestContext(request))
 
-    key = list(rs)[0]
-    return render_to_response('test.html', {'rs': rs}, context_instance=RequestContext(request))
+
+if __name__ == '__main__':
+    print get_sites_for_day(datetime.date.today())
