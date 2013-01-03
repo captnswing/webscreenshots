af270538a3dd07e5fed20c95e1f5df4032301825
Frank Hoffsummer <frank.hoffsummer@gmail.com>
2013-01-03 16:48:56 +0100

    added wsgi.py

---
+++ src/webscreenshots/wsgi.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webscreenshots.settings")
+
+# This application object is used by the development server
+# as well as any WSGI server configured to use this file.
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
