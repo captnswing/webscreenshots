d71bf3bdd3dabece30d7498849eceac82920caeb
Frank Hoffsummer <frank.hoffsummer@gmail.com>
2013-01-07 02:44:33 +0100

    basic date- and site-picker form in place

--- src/webscreenshots/main/views.py
+++ src/webscreenshots/main/views.py
@@ -1,5 +1,6 @@
 #-*- coding: utf-8 -*-
 import os
+
 os.environ["DJANGO_SETTINGS_MODULE"] = "settings"
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
@@ -12,8 +13,16 @@ def get_sites_for_day(selected_day):
     bucket = conn.get_bucket('svti-webscreenshots')
     selected_day = selected_day.strftime("%Y/%m/%d")
     keys = bucket.get_all_keys(prefix=selected_day + "/", delimiter="/")
-    sites = [ key.name.lstrip(selected_day).rstrip("/").replace('|', '/') for key in keys ]
+    sites = [key.name.lstrip(selected_day).rstrip("/").replace('|', '/') for key in keys]
     return sites
+
+
+# from http://stackoverflow.com/a/312464/41404
+def chunks(l, n):
+    """ Yield successive n-sized chunks from l.
+    """
+    for i in xrange(0, len(l), n):
+        yield l[i:i + n]
 
 
 def home(request, pubdate=None):
@@ -22,6 +31,13 @@ def home(request, pubdate=None):
     else:
         y, m, d = pubdate.split('-')
         d = datetime.datetime(int(y), int(m), int(d))
+    keyname = "sites_%s" % d.strftime("%Y-%m-%d")
+    if keyname in request.session:
+        sitesforday = request.session[keyname]
+    else:
+        sitesforday = get_sites_for_day(d)
+        request.session[keyname] = sitesforday
     return render_to_response('home.html', {
+        'sitechunks': chunks(sitesforday, 7),
         'selected_day': d.ctime()
     }, context_instance=RequestContext(request))
