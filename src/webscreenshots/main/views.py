e28a356bf50cf1ea802610c2f9fe6d059467481b
Frank Hoffsummer <frank.hoffsummer@gmail.com>
2013-01-11 20:01:14 +0100

    added sites to get params. alert for non-existing sites

--- src/webscreenshots/main/views.py
+++ src/webscreenshots/main/views.py
@@ -29,6 +29,7 @@ def chunks(l, n):
 def home(request, pubdate=None):
     thumbwidth = request.GET.get("thumbwidth", 220)
     lens = request.GET.get("lens", "on")
+
     firstdataday = datetime.datetime(2013, 1, 3)
     today = datetime.datetime.today()
     if not pubdate:
@@ -43,6 +44,7 @@ def home(request, pubdate=None):
         return HttpResponseRedirect('/%s' % firstdataday.strftime("%Y-%m-%d"))
     if d > today:
         return HttpResponseRedirect('/%s' % today.strftime("%Y-%m-%d"))
+
     keyname = "sites_%s" % d.strftime("%Y-%m-%d")
     if keyname in request.session:
         sitesforday = request.session[keyname]
@@ -50,7 +52,15 @@ def home(request, pubdate=None):
         sitesforday = get_sites_for_day(d)
         request.session[keyname] = sitesforday
 
-    sites = ["aftonbladet.se", "expressen.se", "svt.se/nyheter", "svd.se"]
+    sites = request.GET.get("sites", [])
+    if not sites:
+        sites = ["aftonbladet.se", "expressen.se", "svt.se/nyheter", "svd.se", "dn.se"]
+    else:
+        sites = sites.split(',')
+
+    wrongsites = [ s for s in sites if s not in sitesforday ] or None
+    sites = [ s for s in sites if s in sitesforday ]
+
     offhours = [23, 0, 1, 2, 3, 4, 5, 6]
 
     return render_to_response('home.html', {
@@ -61,5 +71,6 @@ def home(request, pubdate=None):
         'selected_day': d.ctime(),
         'selected_sites': sites,
         'selected_sites_json': json.dumps(sites),
+        'wrongsites_json': json.dumps(wrongsites),
         'allsites': chunks(sitesforday, 8),
     }, context_instance=RequestContext(request))
