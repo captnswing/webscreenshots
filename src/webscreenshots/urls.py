2ca32ab403a4abfbd54c3cbaa3c8c645ee9a19df
Frank Hoffsummer <frank.hoffsummer@gmail.com>
2013-02-12 19:32:52 +0100

    added about page

--- src/webscreenshots/urls.py
+++ src/webscreenshots/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^(?P<pubdate>\d{4}-\d{2}-\d{2})/$', 'main.views.home', name="home-date"),
+    url(r'^about/', TemplateView.as_view(template_name="about.html")),
     url(r'^$', 'main.views.home', name='home'),
     # admin
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
