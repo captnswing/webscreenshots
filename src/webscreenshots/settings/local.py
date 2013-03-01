7a03c99edf634dede152f6ca09447427bfac5746
Frank Hoffsummer <frank.hoffsummer@gmail.com>
2013-03-01 12:26:57 +0100

    excluding google+ widget and google analytics tag in case of DEBUG=True

--- src/webscreenshots/settings/local.py
+++ src/webscreenshots/settings/local.py
@@ -1,4 +1,16 @@
 # settings/local.py
 from .base import *
 DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+TEMPLATE_DEBUG = True
+# from http://djangosnippets.org/snippets/1380/
+from fnmatch import fnmatch
+class glob_list(list):
+    def __contains__(self, key):
+        for elt in self:
+            if fnmatch(key, elt): return True
+        return False
+
+INTERNAL_IPS = glob_list([
+    '127.0.0.1',
+    '10.0.*.*'
+])
