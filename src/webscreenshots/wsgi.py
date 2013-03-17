930a1212fa9c68169cafdd6abbac3f4cabb51a1e
Frank Hoffsummer <frank.hoffsummer@gmail.com>
2013-03-17 10:23:42 +0100

    moved wsgi.py to new location

--- src/webscreenshots/wsgi.py
+++ src/webscreenshots/wsgi.py
@@ -15,8 +15,6 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webscreenshots.settings")
-
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
