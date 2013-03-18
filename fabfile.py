e586b37120dbc2fbf832397204db3e1f0822fe22
Frank Hoffsummer <frank.hoffsummer@gmail.com>
2013-03-18 18:50:42 +0100

    created fab task to remove data for a selected site

--- fabfile.py
+++ fabfile.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
-from fabric.api import cd, prefix, run, env, shell_env, task, local, get
+from fabric.api import cd, prefix, run, env, shell_env, task, local, get, prompt
 import datetime
 from boto.s3.connection import S3Connection
 import os
+from webscreenshots.main.models import WebSite
 
 env.hosts = ['ec2-54-228-243-189.eu-west-1.compute.amazonaws.com']
 env.activate = 'source /opt/webscreenshots/bin/activate'
 env.directory = '/opt/webscreenshots/src/webscreenshots/src/webscreenshots'
 env.shell_env = {'DJANGO_SETTINGS_MODULE': 'webscreenshots.settings.prod'}
 env.use_ssh_config = True
+
+
+def get_env_variable(var_name):
+    """ Get the environment variable or return exception """
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        raise ValueError("Set the %s env variable" % var_name)
 
 
 @task
@@ -27,11 +36,33 @@ def fetch_celeryd_logs():
     #get('/opt/webscreenshots/var/log/celeryd.log*')
 
 
-
 @task
-def remove_site_from_se(sitetoremove):
-    AWS_ACCESS_KEY = os.environ["AWS_ACCESS_KEY"]
-    AWS_SECRET_KEY = os.environ["AWS_SECRET_KEY"]
+def remove_site_from_se(sitetoremove=None):
+    AWS_ACCESS_KEY = get_env_variable("AWS_ACCESS_KEY")
+    AWS_SECRET_KEY = get_env_variable("AWS_SECRET_KEY")
+    if not sitetoremove:
+        sitetuple = list(enumerate(WebSite.objects.all()))
+        nrrange = zip(*sitetuple)[0]
+        for i, s in sitetuple:
+            print "{0:>2}) {1}".format(i+1, s)
+
+    def choiceinrange(input):
+        try:
+            input = int(input)
+        except:
+            raise ValueError("invalid choice '%s'" % input)
+
+        if int(input)-1 in nrrange:
+            return int(input)
+        else:
+            raise ValueError("invalid choice '%s'" % input)
+
+    sitenr = prompt("choose site nr: ", validate=choiceinrange)
+    sitetoremove = sitetuple[sitenr-1][1]
+    yes = prompt("really delete all occurences of '%s' (y/n)?" % sitetoremove, validate=lambda x: x.lower() == 'y')
+    if not yes:
+        print "mission aborted."
+        return
     conn = S3Connection(AWS_ACCESS_KEY, AWS_SECRET_KEY)
     bucket = conn.get_bucket("svti-webscreenshots")
     d = datetime.datetime(2013, 1, 4)
