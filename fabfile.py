e7149384fefdf665160d50810804c6fcb6436a97
Frank Hoffsummer <frank.hoffsummer@gmail.com>
2013-03-26 20:59:58 +0100

    updated fabfile with rsync task

--- fabfile.py
+++ fabfile.py
@@ -6,11 +6,8 @@ from boto.s3.connection import S3Connection
 import os
 from webscreenshots.main.models import WebSite
 
-env.hosts = ['ec2-54-228-243-189.eu-west-1.compute.amazonaws.com']
-env.activate = 'source /opt/webscreenshots/bin/activate'
-env.directory = '/opt/webscreenshots/src/webscreenshots/src/webscreenshots'
-env.shell_env = {'DJANGO_SETTINGS_MODULE': 'webscreenshots.settings.prod'}
 env.use_ssh_config = True
+#env.disable_known_hosts = True
 
 
 def get_env_variable(var_name):
@@ -20,6 +17,31 @@ def get_env_variable(var_name):
     except KeyError:
         raise ValueError("Set the %s env variable" % var_name)
 
+@task
+def prod():
+    env.hosts = ['webscreenshots.captnswing.net']
+    env.activate = 'source /opt/webscreenshots/bin/activate'
+    env.directory = '/opt/webscreenshots/src/webscreenshots/src/webscreenshots'
+    env.shell_env = {'DJANGO_SETTINGS_MODULE': 'webscreenshots.settings.prod'}
+
+@task
+def test():
+    env.hosts = ['test.webscreenshots.captnswing.net']
+    env.activate = 'source /opt/webscreenshots/bin/activate'
+    env.directory = '/opt/webscreenshots/src/webscreenshots/src/webscreenshots'
+    env.shell_env = {'DJANGO_SETTINGS_MODULE': 'webscreenshots.settings.test'}
+
+@task
+def dev():
+    env.hosts = ['ec2-54-228-147-208.eu-west-1.compute.amazonaws.com']
+    env.activate = 'source /opt/webscreenshots/bin/activate'
+    env.directory = '/opt/webscreenshots/src/webscreenshots/src/webscreenshots'
+    env.shell_env = {'DJANGO_SETTINGS_MODULE': 'webscreenshots.settings.test'}
+
+@task
+def deploy():
+    with cd(env.directory), shell_env(**env.shell_env), prefix(env.activate):
+        run("cd ..; hg pull; hg update")
 
 @task
 def dbdump():
@@ -29,12 +51,20 @@ def dbdump():
         run("gzip /tmp/dbbackup_%s.json" % datetime.datetime.today().strftime("%Y-%m-%d"))
         get("/tmp/dbbackup_%s.json.gz" % datetime.datetime.today().strftime("%Y-%m-%d"), local_path="src/webscreenshots/main/fixtures")
 
-
 @task
 def fetch_celeryd_logs():
     local('rsync --times --progress --compress --rsh=/usr/bin/ssh ec2-54-228-243-189.eu-west-1.compute.amazonaws.com:/opt/webscreenshots/var/log/celeryd.log\* celerylogs')
     #get('/opt/webscreenshots/var/log/celeryd.log*')
 
+@task
+def rsync_code():
+    excludes = [".idea", ".hg*", ".vagrant", "build", "dist", "setuptools*", "webscreenshots_kitchen", "distribute*", "celerylogs", "src/webscreenshots.egg-info"]
+    excludes = [ "--exclude=\"{0}\"".format(e) for e in excludes ]
+    excludes = " ".join(excludes)
+    rsync_options = "--progress --compress --rsh=/usr/bin/ssh --recursive --times --perms --links {0}".format(excludes)
+    rsync_cmd = "rsync {0} . {1}:/opt/webscreenshots/src/webscreenshots/".format(rsync_options, env.hosts[0])
+    local(rsync_cmd)
+    run("touch %s/wsgi.py" % env.directory)
 
 @task
 def remove_site_from_se(sitetoremove=None):
@@ -46,16 +76,16 @@ def remove_site_from_se(sitetoremove=None):
         for i, s in sitetuple:
             print "{0:>2}) {1}".format(i+1, s)
 
-    def choiceinrange(input):
+    def choiceinrange(userinput):
         try:
-            input = int(input)
+            userinput = int(userinput)
         except:
-            raise ValueError("invalid choice '%s'" % input)
+            raise ValueError("invalid choice '%s'" % userinput)
 
-        if int(input)-1 in nrrange:
-            return int(input)
+        if int(userinput)-1 in nrrange:
+            return int(userinput)
         else:
-            raise ValueError("invalid choice '%s'" % input)
+            raise ValueError("invalid choice '%s'" % userinput)
 
     sitenr = prompt("choose site nr: ", validate=choiceinrange)
     sitetoremove = sitetuple[sitenr-1][1]
