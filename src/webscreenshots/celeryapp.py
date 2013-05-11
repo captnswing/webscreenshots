8922e64eb923250b6adf8ff8670a6389c62e6eeb
Frank Hoffsummer <frank.hoffsummer@gmail.com>
2013-05-11 09:25:55 +0200

    experimenting with CELERY_ROUTES

--- src/webscreenshots/celeryapp.py
+++ src/webscreenshots/celeryapp.py
@@ -1,33 +1,37 @@
-from __future__ import absolute_import
 from celery import Celery
 from celery.schedules import crontab
 
-celery = Celery('proj.celery',
+
+celery = Celery(
     broker='redis://',
     backend='redis://',
 )
 
+
 # Optional configuration, see the application user guide.
 celery.conf.update(
-    CELERY_TIMEZONE = 'Europe/Stockholm',
+    CELERY_TIMEZONE='Europe/Stockholm',
     CELERY_IMPORTS='webscreenshots.celerytasks',
-    CELERY_DISABLE_RATE_LIMITS = True,
+    CELERY_DISABLE_RATE_LIMITS=True,
     CELERY_TASK_RESULT_EXPIRES=3600,
     BROKER_TRANSPORT_OPTIONS={'visibility_timeout': 3600},
+    # http://stackoverflow.com/a/10756040/41404
+    # CELERY_ROUTES={
+    #     "webscreenshots.celerytasks.webscreenshots": {"queue": "phantomjs"},
+    #     "webscreenshots.celerytasks.fetch_webscreenshot": {"queue": "phantomjs"}
+    # },
     # http://docs.celeryproject.org/en/latest/userguide/periodic-tasks.html#crontab-schedules
-    CELERYBEAT_SCHEDULE = {
+    CELERYBEAT_SCHEDULE={
         'runs-every-5-minutes': {
             'task': 'webscreenshots.celerytasks.webscreenshots',
             'schedule': crontab(minute='*/5', hour='7-22'),
-#            'schedule': crontab(minute='*/5'),
-           },
+        },
         'runs-every-hour': {
             'task': 'webscreenshots.celerytasks.webscreenshots',
             'schedule': crontab(minute='0', hour='23-6'),
         }
     }
 )
-
 
 if __name__ == '__main__':
     celery.start()
