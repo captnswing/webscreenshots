27ad2b6a24a1897df17e20ce3b63b0d8a8d708dd
Frank Hoffsummer <frank.hoffsummer@gmail.com>
2013-05-11 09:25:20 +0200

    added settings.vagrant. switched to settings.frank in load_urls script

--- src/webscreenshots/load_urls.py
+++ src/webscreenshots/load_urls.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 import os
-os.environ["DJANGO_SETTINGS_MODULE"] = 'webscreenshots.settings.local'
+os.environ["DJANGO_SETTINGS_MODULE"] = 'webscreenshots.settings.frank'
 from webscreenshots.main.models import WebSite
 import csv
 
