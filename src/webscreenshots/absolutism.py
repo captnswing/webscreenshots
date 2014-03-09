bb82ab62d30123a2cf45dc70ba45c089699a5d46
Frank Hoffsümmer <frank.hoffsummer@gmail.com>
2014-03-09 10:50:05 +0100

    wrangling with relative urls

--- src/webscreenshots/absolutism.py
+++ src/webscreenshots/absolutism.py
@@ -2,26 +2,54 @@
 #-*- coding: utf-8 -*-
 import unittest
 from pyquery import PyQuery as pq
+from fetch_html import download_css, make_absolute, add_base_tag
 
 
 class TestUrlConversion(unittest.TestCase):
 
     def setUp(self):
-        self.testhtml = open('data/sverigesradio.se_blekinge.html', 'r').read()
-        self.d = pq(self.testhtml)
-        self.testcss = open('data/sr.responsive.min.css', 'r').read()
+        self.base_url = "http://sverigesradio.se/blekinge"
+        self.testhtml = open('main/fixtures/simpletest.html', 'r').read()
+        self.testcss = open('main/fixtures/test.css', 'r').read()
+        self.d = pq(self.testhtml, parser='html')
 
-    def test_script_urls(self):
-        # finds all script tags that have a src attribute whose value starts with '/'
-        for fixs in self.d('script[src^="/"]'):
-            print "*"*50
-            absolutePath = "http://sverigesradio.se" + pq(fixs).attr['src']
-            pq(fixs).attr['src'] = absolutePath
-            #print pq(fixs).attr['src']
-        #print self.d.html()
+    def test_make_absolute_script_urls(self):
+        d = make_absolute(self.d, self.base_url)
+        #print d.outerHtml()
+
+    def test_add_base_tag(self):
+        # basic test html
+        test_html = """<html><head></head><body></body></html>"""
+        test_html_with_base = """<html><head><base href="http://test123"></head><body></body></html>"""
+        # parse test html
+        without_base = pq(test_html, parser='html')
+        with_base = pq(test_html_with_base, parser='html')
+        # confirm input conditions
+        self.assertIsNone(without_base.attr['href'])
+        self.assertEquals(with_base('base').attr['href'], "http://test123")
+        # add / update base tage
+        d_with_base = add_base_tag(with_base, self.base_url)
+        d_without_base = add_base_tag(without_base, self.base_url)
+        # check that base tag exists in head and that it has correct href attribute
+        self.assertEquals(d_with_base('base').attr['href'], self.base_url)
+        self.assertEquals(d_without_base('base').attr['href'], self.base_url)
 
     def test_css_urls(self):
-        print self.testcss
+        pass
+        #print self.testcss
+
+    def test_parse_html(self):
+        testhtml = open('main/fixtures/simpletest.html', 'r').read()
+        d = pq(testhtml, parser='html')
+        #print d.outerHtml()
+
+
+
+def main():
+    filename = "data/aftonbladet.se.html"
+    download_css(filename)
+
 
 if __name__ == '__main__':
     unittest.main()
+    #main()
--- src/webscreenshots/fetch_html.py
+++ src/webscreenshots/fetch_html.py
@@ -9,6 +9,47 @@ import requests
 from urlparse import urlparse
 
 
+def download_css(filename):
+    d = pq(open(filename).read())
+    for fixl in d('link[href^="/"]'):
+        print pq(fixl).attr['href']
+
+
+def add_base_tag(d, base_url):
+    # add (or update) base tag!
+    if d('base'):
+        d('base').attr['href'] = base_url
+    else:
+        d('head').append('<base href="{}"'.format(base_url))
+    return d
+
+def make_absolute(d, base_url):
+
+    # finds all script tags that have a src attribute whose value starts with '/'
+    for fixs in d('script[src^="/"]'):
+        # prepend base_url to the src value
+        absolutepath = base_url + pq(fixs).attr['src']
+        pq(fixs).attr['src'] = absolutepath
+
+    # from http://pythonhosted.org/pyquery/tips.html
+    d.make_links_absolute(base_url=base_url)
+
+    # finds all link tags that have a href attribute whose value starts with '/'
+    # for fixl in d('link[href^="/"]'):
+    #     absolutepath = base_url + pq(fixl).attr['href']
+    #     pq(fixl).attr['href'] = absolutepath
+    #     print absolutepath
+    return d
+
+
+def write_html(d, html_filename):
+    """write html to file"""
+    html = codecs.open('data/{}'.format(html_filename), 'wb', 'utf-8')
+    print 'write: {}'.format(html_filename)
+    html.write(d.outer_html())
+    html.close()
+
+
 def save_html(website):
     """thread worker function"""
 
@@ -20,38 +61,23 @@ def save_html(website):
 
     # fetch html
     try:
-        d = pq(website.url)
+        d = pq(website.url, parser='html')
     except ValueError:
         r = requests.get(website.url)
         cleanhtml = r.text.replace("""<?xml version="1.0" encoding="UTF-8"?>""", "")
-        d = pq(cleanhtml)
+        d = pq(cleanhtml, parser='html')
 
-    # relative -> absolute
-    d('head').append('<base href="{}"'.format(base_url))
-    # finds all script tags that have a src attribute whose value starts with '/'
-    for fixs in d('script[src^="/"]'):
-        absolutepath = base_url + pq(fixs).attr['src']
-        pq(fixs).attr['src'] = absolutepath
-    # finds all link tags that have a href attribute whose value starts with '/'
-    for fixl in d('link[href^="/"]'):
-        absolutepath = base_url + pq(fixl).attr['href']
-        pq(fixl).attr['href'] = absolutepath
-        print absolutepath
-
-    # write html to file
+    d = make_absolute(d, base_url)
     html_filename = '{}.html'.format(canonical_url.replace('/', '_'))
-    html = codecs.open('data/{}'.format(html_filename), 'wb', 'utf-8')
-    print 'write: {}'.format(html_filename)
-    html.write(d.html())
-    html.close()
+    write_html(d, html_filename)
     return
 
 
 def main():
     jobs = []
     for ws in WebSite.objects.all():
-        if not ws.url == "http://sverigesradio.se/sida/default.aspx?programid=125":
-            continue
+        # if not ws.title == "Aftonbladet":
+        #     continue
         p = multiprocessing.Process(target=save_html, args=(ws,))
         jobs.append(p)
         p.start()
