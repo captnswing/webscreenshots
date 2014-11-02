8c5b5ac90af4cb6d022ff9003aa2f99c2eea18e0
Frank Hoffsümmer <hoffsummer@spotify.com>
2014-11-02 13:45:38 +0100

    working capture script for local html files

--- bin/capture_local.js
+++ bin/capture_local.js
@@ -1,33 +1,22 @@
 var casper = require('casper').create({
-    verbose: true,
+    verbose: false,
     logLevel: "info",
     waitTimeout: 40000,
     timeout: 40000,
-    pageSettings: {
-        loadPlugins: true,
-        loadImages: true,
-        javascriptEnabled: true
-    },
-    viewportSize: {width: 1280, height: 800}
+    userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:33.0) Gecko/20100101 Firefox/33.0'
 });
 
-//https://gist.github.com/nhoizey/4060568
-var fs = require('fs');
-if (casper.cli.args.length < 1) {
+if (casper.cli.args.length != 1) {
     casper
-        .echo("Usage:\n$ casperjs capture_local.js <htmlfiles>")
-        .exit(1)
-    ;
-} else {
-    var htmlfiles = casper.cli.args.filter(function (f) {
-        return /.*\.html/.test(f);
-    });
+        .echo("Usage:\n$ casperjs capture_local.js <htmlfile>")
+        .exit(1);
 }
 
-// TODO: insert <base> tag into <head>
-// http://stackoverflow.com/a/589845/41404
+var fs = require('fs');
+var htmlfile = casper.cli.args[0];
+var jpgfile = htmlfile.replace('.html', '.jpg');
 
-casper.userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:20.0) Gecko/20100101 Firefox/20.0');
+casper.userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:33.0) Gecko/20100101 Firefox/33.0');
 
 casper.on('http.status.301', function (resource) {
     this.log('Hey, this one is 301: ' + resource.url, 'warning');
@@ -41,14 +30,25 @@ casper.on('http.status.500', function (resource) {
     casper.exit(1);
 });
 
-casper.each(htmlfiles, function (casper, htmlfile) {
-    casper.start('file:////Users/hoffsummer/Development/webscreenshots/src/webscreenshots/' + htmlfile, function () {
-        this.echo('capturing file ' + htmlfile);
-        this.viewport(1280, 800);
-        this.evaluate(function () {
-            document.body.style.backgroundColor = '#fff';
-        });
-        this.capture(htmlfile+'.png');
+casper.start();
+
+casper.then(function () {
+    this.echo('rendering from file ' + htmlfile);
+    this.page.content = fs.read(htmlfile);
+});
+
+casper.then(function () {
+    this.viewport(1280, 800);
+    this.evaluate(function () {
+        document.body.style.backgroundColor = '#fff';
+    });
+});
+
+casper.then(function () {
+    this.echo('capturing to file ' + jpgfile);
+    this.capture(jpgfile, undefined, {
+        format: 'jpg',
+        quality: 98
     });
 });
 
